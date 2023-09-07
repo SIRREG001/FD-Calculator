2c154786b0043c7b964ff6517b676544eed4c884
SIRREG001 <udochukwureginald78@gmail.com>
2023-09-07 13:28:13 +0100

    Create the skeleton of the calculator

---
+++ script.js
@@ -0,0 +1,20 @@
+function calculateMaturityAmount() {
+    //Get input values from the form elements
+
+    const principle = parseFloat(document.getElementById('principle').value)
+    const interestRate = parseFloat(document.getElementById('interestRate').value)
+    const tenure = parseFloat(document.getElementById('tenure').value)
+
+
+    //perform the calculation
+
+    const maturityAmount = principle + (principle * interestRate * tenure) / 100;
+
+    //display the result
+
+    document.getElementById('result').innerText = `Maturity Amount: ${maturityAmount.toFixed(2)}`;
+}
+
+//Attach event listener to the calculator button
+
+document.getElementById('calculatorBtn').addEventListener('click', calculateMaturityAmount);
